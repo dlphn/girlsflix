d779a31d40dd2da63ab71ad5c141472ae07f5374
Delphine Shi <shi.delphine@gmail.com>
2018-10-12 16:58:03 +0200

    remove api key

--- back-end/src/main/java/server/MainTest.java
+++ back-end/src/main/java/server/MainTest.java
@@ -8,10 +8,11 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		Keys apiKey = new Keys();
 		
 		ConnectionWS connection = new ConnectionWS();
 		try {
-			connection.connect("series", "https://api.themoviedb.org/3/tv/popular?api_key=dbd1261470d6dfededebf40207478bef&language=en-US&page=1");
+			connection.connect("series", "https://api.themoviedb.org/3/tv/popular?api_key=" + apiKey.getApiKey() + "&language=en-US&page=1");
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
