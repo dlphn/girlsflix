6ffcb31f0f9a73e16c3ef0bdc667fb3e45fe3824
Sophie Dambricourt <sophie@MacBook-Pro-de-Sophie.local>
2018-10-12 12:08:42 +0200

    Serie creation and TypeSerie creation

--- back-end/src/main/java/server/Serie.java
+++ back-end/src/main/java/server/Serie.java
@@ -1,9 +1,99 @@
 package server;
 
-public class Serie {
+import java.time.LocalDate;
 
-	public Serie() {
-		// TODO Auto-generated constructor stub
+public class Serie {
+	
+	private int id;
+	private String title;
+	private TypeSerie typeSerie;
+	private String summary;
+	private LocalDate creationDate;
+	private int rating;
+	private boolean allowed;
+	//private List<Season> seasons;
+	
+	public Serie(int id, String title, TypeSerie typeSerie, String summary, LocalDate creationDate) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.typeSerie = typeSerie;
+		this.summary = summary;
+		this.creationDate = creationDate;
+		this.allowed = true;
+		this.rating = 0;
 	}
+	
+	/*public void addSeason(Season season) {
+		seasons.add(season);
+		
+	}*/
+	
+	public void show() {
+		//fonction à appeler quand l'utilisateur sélectionnera une série et qu'on souhaitera afficher les détails de la série
+		
+	}
+	
+	
+	/*******************/
+	/*Getters & Setters*/
+	/*******************/
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public TypeSerie getTypeSerie() {
+		return typeSerie;
+	}
+	
+	public void setTypeSerie(TypeSerie typeSerie) {
+		this.typeSerie = typeSerie;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public LocalDate getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDate creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public boolean isAllowed() {
+		return allowed;
+	}
+
+	public void setAllowed(boolean allowed) {
+		this.allowed = allowed;
+	}
+	
 
 }
