440abd2ca67787d4ac5c8d8a6cc0798b869f73b7
Delphine Shi <shi.delphine@gmail.com>
2018-10-30 22:02:38 +0100

    static seriesList in Data instead of Visualization

---
+++ GirlsFlix/src/main/java/com/gfx/domain/series/Data.java
@@ -0,0 +1,75 @@
+package com.gfx.domain.series;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Data {
+	private static List<Serie> seriesList;
+	
+	public static List<Serie> getListSeries() {
+		return seriesList;
+	}
+
+	public static void setListSeries(List<Serie> series) {
+		seriesList = series;
+	}
+	
+	public void showSeries() {
+		try {
+			System.out.println("The series available are : ");
+			for (Serie s : seriesList){
+				System.out.println(s.info());
+				//System.out.println("\n\n");
+			} 
+		} catch (NullPointerException e) {
+			System.out.println("size is :   No Series for the moment.");}
+	}
+	
+	public static List<Serie> pickNRandom(int n) {
+	    List<Serie> copy = new ArrayList<Serie>(seriesList);
+	    Collections.shuffle(copy);
+	    return copy.subList(0, n);
+	}
+	
+	/**
+	 * Runs through the whole list of series to return the series which title contains the search query (case insensitive)
+	 * 
+	 * @param query		the user search request
+	 * @return 			the list of series that match the search query
+	 */
+	public static List<Serie> search(String query) {
+		List<Serie> result = seriesList.stream()
+			     .filter(item -> 
+			     	Pattern.compile(Pattern.quote(query), Pattern.CASE_INSENSITIVE).matcher(item.getTitle()).find())
+			     .collect(Collectors.toList());
+		return result;
+	}
+	
+	/**
+	 * Runs through the list of series to return those of the same genre as the query
+	 * @param query		the genre to filter on
+	 * @return 			the list of series of the genre specified
+	 */
+	public static List<Serie> searchGenre(String query) {
+		List<Serie> result = seriesList.stream()
+			     .filter(item -> item.getSerieGenre().contains(query))
+			     .collect(Collectors.toList());
+		return result;
+	}
+	
+	public static Serie getById(int id) {
+		Serie searched = null;
+		for (Serie s : seriesList) {
+			if(s.getId() == id) {
+				 searched = s;
+			}
+		}
+		return searched;
+	}
+}
--- GirlsFlix/src/main/java/com/gfx/service/SerieFactory.java
+++ GirlsFlix/src/main/java/com/gfx/service/SerieFactory.java
@@ -5,18 +5,16 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 
+import com.gfx.domain.series.Data;
 import com.gfx.domain.series.Episode;
 import com.gfx.domain.series.Season;
 import com.gfx.domain.series.Serie;
-import com.sun.javafx.scene.paint.GradientUtils.Parser;
 
 import org.bson.Document;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import java.io.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Service
@@ -24,6 +22,47 @@ public class SerieFactory {
 	ArrayList<Serie> seriesList =  new ArrayList<Serie>(); // A changer plus tard en HashMap par exemple
 	
 	public SerieFactory() {
+		if (Data.getListSeries() == null) {
+			initSeries();
+		}
+	}
+	
+	public void initSeries() {
+		SerieDB.connect();
+        
+        JSONParser parser = new JSONParser();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        
+        List<Document> documents = SerieDB.find("series");
+        
+    	for (Document doc : documents) {
+			try {
+				JSONObject jsnObj = (JSONObject) parser.parse(doc.toJson());
+				
+    			JSONArray genres = (JSONArray) jsnObj.get("serieType");
+    			List<String> serieType = new ArrayList<String>();
+    			for (int i = 0; i < genres.size(); i++){
+                    serieType.add(genres.get(i).toString());
+                }
+    			
+    			int serieId = Integer.parseInt(((JSONObject) jsnObj.get("id")).get("$numberLong").toString());
+				List<Season> seasons = getSeasons(serieId);
+    			
+    			Serie serie = new Serie(
+    					serieId, //id
+    					jsnObj.get("title") != null ? jsnObj.get("title").toString() : "", //title
+    					serieType,//serieGenres
+    					jsnObj.get("summary") != null ? jsnObj.get("summary").toString() : "", //summary
+    					jsnObj.get("date") != null ? LocalDate.parse((CharSequence) jsnObj.get("date"), formatter) : null, //date
+						jsnObj.get("imageLink") != null ? jsnObj.get("imageLink").toString() : "", //image
+    					seasons);//image
+    			seriesList.add(serie);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+		Data.setListSeries(seriesList);
 	}
 	
 	public List<Serie> getSeries() {
